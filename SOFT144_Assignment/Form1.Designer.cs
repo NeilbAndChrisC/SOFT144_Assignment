980ddfcadbae00e8c4aec3820ab88fb2c041033d
neilb17 <neil.bennett@students.plymouth.ac.uk>
2015-03-04 20:30:25 +0000

    hope this works
    delete module almost finished

---
+++ SOFT144_Assignment/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace SOFT144_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moduleDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileContentsTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.editButton = new System.Windows.Forms.Button();
+            this.titleTexBox = new System.Windows.Forms.TextBox();
+            this.ModuleTitlelistView1 = new System.Windows.Forms.ListView();
+            this.StageTextBox = new System.Windows.Forms.TextBox();
+            this.DeletButton = new System.Windows.Forms.Button();
+            this.AddModuleButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InClassTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(861, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.moduleDetailsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.saveAsToolStripMenuItem.Text = "&Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // moduleDetailsToolStripMenuItem
+            // 
+            this.moduleDetailsToolStripMenuItem.Name = "moduleDetailsToolStripMenuItem";
+            this.moduleDetailsToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.moduleDetailsToolStripMenuItem.Text = "Module Details";
+            // 
+            // fileContentsTextBox
+            // 
+            this.fileContentsTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileContentsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileContentsTextBox.Location = new System.Drawing.Point(360, 330);
+            this.fileContentsTextBox.Multiline = true;
+            this.fileContentsTextBox.Name = "fileContentsTextBox";
+            this.fileContentsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.fileContentsTextBox.Size = new System.Drawing.Size(414, 116);
+            this.fileContentsTextBox.TabIndex = 1;
+            this.fileContentsTextBox.TextChanged += new System.EventHandler(this.fileContentsTextBox_TextChanged);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "Text Files (.txt) | *.txt";
+            this.openFileDialog.InitialDirectory = "\"C:\\Users\\chriscarter\\Documents\\";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Text Files (.txt) |*.txt| All Files|*.*";
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(97, 380);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(121, 30);
+            this.editButton.TabIndex = 4;
+            this.editButton.Text = "EDIT MODULE";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // titleTexBox
+            // 
+            this.titleTexBox.BackColor = System.Drawing.SystemColors.Control;
+            this.titleTexBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleTexBox.Location = new System.Drawing.Point(100, 63);
+            this.titleTexBox.Multiline = true;
+            this.titleTexBox.Name = "titleTexBox";
+            this.titleTexBox.Size = new System.Drawing.Size(533, 29);
+            this.titleTexBox.TabIndex = 5;
+            this.titleTexBox.TextChanged += new System.EventHandler(this.titleTexBox_TextChanged);
+            // 
+            // ModuleTitlelistView1
+            // 
+            this.ModuleTitlelistView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ModuleTitlelistView1.LabelWrap = false;
+            this.ModuleTitlelistView1.Location = new System.Drawing.Point(97, 152);
+            this.ModuleTitlelistView1.Name = "ModuleTitlelistView1";
+            this.ModuleTitlelistView1.Size = new System.Drawing.Size(121, 177);
+            this.ModuleTitlelistView1.TabIndex = 6;
+            this.ModuleTitlelistView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // StageTextBox
+            // 
+            this.StageTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.StageTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StageTextBox.Location = new System.Drawing.Point(658, 63);
+            this.StageTextBox.Multiline = true;
+            this.StageTextBox.Name = "StageTextBox";
+            this.StageTextBox.Size = new System.Drawing.Size(100, 29);
+            this.StageTextBox.TabIndex = 7;
+            // 
+            // DeletButton
+            // 
+            this.DeletButton.Location = new System.Drawing.Point(97, 344);
+            this.DeletButton.Name = "DeletButton";
+            this.DeletButton.Size = new System.Drawing.Size(121, 30);
+            this.DeletButton.TabIndex = 8;
+            this.DeletButton.Text = "DELETE MODULE";
+            this.DeletButton.UseVisualStyleBackColor = true;
+            this.DeletButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // AddModuleButton
+            // 
+            this.AddModuleButton.Location = new System.Drawing.Point(97, 416);
+            this.AddModuleButton.Name = "AddModuleButton";
+            this.AddModuleButton.Size = new System.Drawing.Size(121, 30);
+            this.AddModuleButton.TabIndex = 9;
+            this.AddModuleButton.Text = "ADD MODULE";
+            this.AddModuleButton.UseVisualStyleBackColor = true;
+            this.AddModuleButton.Click += new System.EventHandler(this.AddModuleButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "MODULE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(229, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "IN CLASS TEST";
+            // 
+            // InClassTextBox
+            // 
+            this.InClassTextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.InClassTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InClassTextBox.Location = new System.Drawing.Point(233, 152);
+            this.InClassTextBox.Multiline = true;
+            this.InClassTextBox.Name = "InClassTextBox";
+            this.InClassTextBox.Size = new System.Drawing.Size(121, 177);
+            this.InClassTextBox.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 595);
+            this.Controls.Add(this.InClassTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AddModuleButton);
+            this.Controls.Add(this.DeletButton);
+            this.Controls.Add(this.StageTextBox);
+            this.Controls.Add(this.ModuleTitlelistView1);
+            this.Controls.Add(this.titleTexBox);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.fileContentsTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Stage Management App";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.TextBox fileContentsTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem moduleDetailsToolStripMenuItem;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.TextBox titleTexBox;
+        private System.Windows.Forms.ListView ModuleTitlelistView1;
+        private System.Windows.Forms.TextBox StageTextBox;
+        private System.Windows.Forms.Button DeletButton;
+        private System.Windows.Forms.Button AddModuleButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InClassTextBox;
+    }
+}
+
---
+++ SOFT144_Assignment/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+/**********************************/
+/* Neil Bennett and Chris Carter */ 
+/*           Feb 2015           */
+/*******************************/
+
+namespace SOFT144_Assignment
+{
+    public partial class Form1 : Form
+    {
+       
+        private string fileName;
+        private string currentDirectoryName;
+        public Form1()
+         
+   
+        {
+            InitializeComponent();
+        }
+       
+        //code below is to open the open dialog box
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string onlyFileName;
+            string directoryName;
+            string driveLetter;
+
+            if (openFileDialog.ShowDialog() != DialogResult.Cancel)
+            {
+
+                currentDirectoryName = openFileDialog.InitialDirectory;
+
+                fileName = openFileDialog.FileName;
+
+                onlyFileName = openFileDialog.SafeFileName;
+
+                directoryName = Path.GetDirectoryName(fileName);
+
+                driveLetter = directoryName.Split(Path.VolumeSeparatorChar)[0];
+
+                LoadFile();
+
+            } // end if
+
+        }// end open tool strip menu
+       
+        private void LoadFile()
+        {
+            string[] wordArray;
+            string lineFromFile;
+            StreamReader reader;
+
+            fileContentsTextBox.Clear();
+
+            try
+            {
+                using (reader = new StreamReader(fileName))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        lineFromFile = reader.ReadLine();
+
+                        fileContentsTextBox.AppendText(lineFromFile);
+                        wordArray = lineFromFile.Split(',');
+                        string moduleTitle = wordArray[0];
+                        ModuleTitlelistView1.Items.Add(new ListViewItem(moduleTitle));
+
+                            //if txt box is empty then fill it
+                           //with txt from the array
+                            if (titleTexBox.Text == string.Empty)
+                            {
+                                string title = wordArray[1];
+                                titleTexBox.Text = title;
+                            }//end if
+
+                            //if txt box is empty then fill it
+                            //with txt from the array
+                            if (StageTextBox.Text == string.Empty)
+                            {
+                                string stage = wordArray[2];
+                                StageTextBox.Text = stage;
+                            }//end if
+
+                        //if txt box is empty then fill it
+                        //with txt from the array
+                            if (InClassTextBox.Text == string.Empty)
+                            {
+                                string ClassText = wordArray[11];
+                                InClassTextBox.Text = ClassText;
+                            }//end if
+                    }
+                }
+            }
+               //if the file is not their
+                //then tell user
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("File not found ");
+            }
+
+            }//end load file
+       
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fileName != null)
+            {
+                SaveFile();
+            } //end if    
+        }//end save tool strip
+
+        private void SaveFile()
+        {
+            string textToSave;
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    textToSave = fileContentsTextBox.Text;
+
+                    writer.WriteLine(textToSave);
+                }
+            }
+
+            catch (IOException ex)
+            {
+                MessageBox.Show("Error writing to file", "File Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+}
+        //code for save as below
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.InitialDirectory = currentDirectoryName;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = saveFileDialog.FileName;
+
+                SaveFile();
+            }
+
+
+        }
+
+        //code below hides the 'blinking curser' in contents
+        //needs more work!!!!!!!!!!
+        private void fileContentsTextBox_TextChanged(object sender, EventArgs e)
+        {
+            ActiveControl = null;
+        }//end file contents text box
+
+        //edit button code below
+        private void editButton_Click(object sender, EventArgs e)
+        {
+
+        }//end edit button
+
+       //ignore code below
+        private void titleTexBox_TextChanged(object sender, EventArgs e)
+        {
+             
+         //ignore  
+        }//end 
+
+        //add file code below
+        private void AddModuleButton_Click(object sender, EventArgs e)
+        {
+            //clear all text from text boxes
+            fileContentsTextBox.Clear();
+            StageTextBox.Clear();
+            titleTexBox.Clear();
+            ModuleTitlelistView1.Clear();
+            InClassTextBox.Clear();
+
+            //need to show the two text box at the top of the form
+            if (fileContentsTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter text!");
+            }
+          
+
+        }//end add module button
+
+        //delete module
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            //give the user an option of not deleting
+            string MessageBoxContent = "Do you really want to delete the file!";
+            //title on the message box
+            string MessageBoxTitle = "WARNING";
+           
+
+            DialogResult dialogResult = MessageBox.Show(MessageBoxContent, MessageBoxTitle, MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {//start if
+                File.Exists(fileName);
+                {
+                    try
+                    {//start try
+
+                        File.Delete(fileName);// need a try and catch here incase a user has no file selected
+                        //clear all text from text boxes
+                        fileContentsTextBox.Clear();
+                        StageTextBox.Clear();
+                        titleTexBox.Clear();
+                        ModuleTitlelistView1.Clear();
+                        InClassTextBox.Clear();
+                    }//end try
+                    catch (Exception ex)
+                    {//start catch
+                        MessageBox.Show("No File");
+                    }//end catch
+
+              
+                }
+            }//end if
+            //if NO then close message box
+            else if (dialogResult == DialogResult.No)
+            {
+                //close message box and do nothing
+            }
+
+            
+        }//end deletButton
+
+    }
+}
